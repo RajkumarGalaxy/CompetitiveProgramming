4eb389da8cc6c404f39747e57f29d37be5ecd84a
Rajkumar Lakshmanamoorthy <rajkumar.galaxy@gmail.com>
2021-07-28 12:43:54 +0530

    Update 100 Reversort.py
    Added file description

--- 100 Reversort.py
+++ 100 Reversort.py
@@ -1,3 +1,20 @@
+"""
+Created on Wed Jul 28 10:03:48 2021
+Python Implementation
+
+NPTEL 
+Getting Started with Competitive Programming
+Week 1
+Mod 1
+Reversort
+https://www.craft.do/s/AcEMNb4NLdHlQo/b/66FE59A3-D17C-45C9-9C2A-A1C4C76A99E9/Module_1._Reversort
+
+by
+https://rajkumargalaxy.github.io/profile
+
+Submission successful at https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d0a5c
+"""
+
 T = int(input())
 arrays = []
 for i in range(T):
